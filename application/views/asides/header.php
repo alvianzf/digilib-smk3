3d31864e87a25dc77ce79c9d8130138cd0a842ef
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-10-14 21:30:31 +0700

    title fix

--- application/views/asides/header.php
+++ application/views/asides/header.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Penjualan Bata</title>
+  <title>Digital Library SMKN 3 Tanjungpinang</title>
 
   <!-- Custom fonts for this template-->
   <link href="assets/templates/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
